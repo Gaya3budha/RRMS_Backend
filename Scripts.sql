78f58d731b5b3ccb6b7392ab66d66c6d47ddeb78
Gaya3budha <budhagayatri95@gmail.com>
2025-03-16 17:30:46 +0530

    sql insert script

---
+++ Scripts.sql
@@ -0,0 +1,5 @@
+-- scripts to insert into roles table
+insert into users_role values(1,'Admin','Y',current_date,current_date)
+(2,'User','Y',current_date,current_date),
+(3,'Viewer','Y',current_date,current_date),
+(4,'ContentManager','Y',current_date,current_date);
