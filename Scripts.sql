44d8e160de90ea5f152ef7e09a6454230ba2ee46
Gaya3budha <budhagayatri95@gmail.com>
2025-03-19 23:35:04 +0530

    updated scripts

--- Scripts.sql
+++ Scripts.sql
@@ -1,11 +1,11 @@
 -- scripts to insert into roles table
-insert into users_role values(1,'Admin','Y',current_date,current_date)
+insert into mdm_role values(1,'Admin','Y',current_date,current_date),
 (2,'User','Y',current_date,current_date),
 (3,'Viewer','Y',current_date,current_date),
 (4,'ContentManager','Y',current_date,current_date);
 
 -- scripts to insert into Division master table
-insert into users_divisionmaster values(1,'Narcotics','Y',current_date),
+insert into mdm_divisionmaster values(1,'Narcotics','Y',current_date),
 (2,'Cybercrime','Y',current_date),
  (3,'Economic Offenses','Y',current_date),
 (4,'Anti Human Trafficking','Y',current_date);
