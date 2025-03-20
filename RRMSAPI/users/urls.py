b0011acc4544ffde9bec3a5bb72c902ad0de9d95
Gaya3budha <budhagayatri95@gmail.com>
2025-03-21 00:29:20 +0530

    update role end point added

--- RRMSAPI/users/urls.py
+++ RRMSAPI/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateUserView,UserListView
+from .views import CreateUserView,UserListView,UpdateUserView
 
 urlpatterns = [
     path('',UserListView.as_view(),name='user-list'), #path for fetching all the users
-    path('create/', CreateUserView.as_view(), name='create_user')
+    path('create/', CreateUserView.as_view(), name='create_user'),
+    path('update-user/<int:kgid_user>/', UpdateUserView.as_view(), name='update_user')
+
 ]
--- RRMSAPI/users/views.py
+++ RRMSAPI/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
-
+from mdm.models import Role
 # Create your views here.
 class UserListView(APIView):
     def get(self,request,*args,**kwargs):
@@ -17,10 +17,46 @@ class CreateUserView(APIView):
         serializer = UserSerializer(data=request.data)
 
         if serializer.is_valid():
-            user = serializer.save() 
+            user = serializer.save()
             return Response(serializer.data,status = status.HTTP_201_CREATED)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UpdateUserView(APIView):
+    def patch(self,request,kgid_user,*args,**kwargs):
+        try:
+            # getting the user based on kgid
+            user = User.objects.get(kgid = kgid_user)
+
+            # blank dictinary object
+            updated_data = {}
+
+            # checking if roleId is present in request body or not
+            if 'roleId' in request.data:
+                role_id = request.data['roleId']
+
+                # fetching the passed roleId in role table
+                new_role = Role.objects.get(roleId=role_id)
+
+                # assigning to dict object
+                updated_data['role']= new_role
+
+                
+                for key, value in updated_data.items():
+                    setattr(user, key, value)
+
+                user.save()
+
+                serializer = UserSerializer(user,  context={'request': request})
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "roleId field is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        except Role.DoesNotExist:
+            return Response({"error": "Role not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 
