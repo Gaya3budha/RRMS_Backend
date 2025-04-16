9eaacb214570418e6f7436b54254f83f09e1391e
Gaya3budha <budhagayatri95@gmail.com>
2025-04-17 01:04:08 +0530

    added env file

--- RRMSAPI/RRMSAPI/settings.py
+++ RRMSAPI/RRMSAPI/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,13 +84,21 @@ WSGI_APPLICATION = 'RRMSAPI.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'RRMS',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
     'default': {
        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'RRMS',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
--- Scripts.sql
+++ Scripts.sql
@@ -4,7 +4,26 @@ insert into mdm_role values(1,'Admin','Y',current_date,current_date),
 (4,'ContentManager','Y',current_date,current_date);
 
 -- scripts to insert into Division master table
-insert into mdm_divisionmaster values(1,'Narcotics','Y',current_date),
-(2,'Cybercrime','Y',current_date),
- (3,'Economic Offenses','Y',current_date),
-(4,'Anti Human Trafficking','Y',current_date);
+insert into mdm_divisionmaster values(1,'CID','Y',current_date),
+(2,'Cyber Crime and Narcotics','Y',current_date),
+ (3,'Economic Offences','Y',current_date),
+(4,'Forest Cell.CID','Y',current_date),
+(5,'Administration','Y',current_date),
+(6,'Narcotics & Organized Crime Division','Y',current_date),
+(7,'Cyber Crime Division','Y',current_date),
+(8,'Deposit Fraud Investigation Division','Y',current_date),
+(9,'Special Enquiry Division','Y',current_date),
+(10,'Criminal Intelligence Unit','Y',current_date),
+(11,'Financial Intelligence Unit','Y',current_date),
+(12,'Homicide and Burglary','Y',current_date),
+(13,'Anti-Human Traffic Unit','Y',current_date),
+(14,' Cyber Training and Research Unit','Y',current_date),
+(15,'Deposit Fraud Investigation Division Under DIGP Economic Officences','Y',current_date);
+
+
+
+
+insert into mdm_designationmaster values(1,'Director General of Police','Y',current_date),
+(2,'Addl. Director General of Police','Y',current_date),
+ (3,'Deputy Inspector General of Police','Y',current_date),
+(4,'Superintendent of Police','Y',current_date);
