6f60890e8ba46757fc71d85835e2a8de9e6316f3
Gaya3budha <budhagayatri95@gmail.com>
2025-05-03 02:28:35 +0530

    admin portal user chnages

--- RRMSAPI/mdm/admin.py
+++ RRMSAPI/mdm/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import Role,DivisionMaster, FileType, FileClassification, CaseStatus
+from .models import Role,DivisionMaster, FileType, FileClassification, CaseStatus, DesignationMaster
 from django.contrib.auth.models import  Permission
 
 # Register your models here.
-admin.site.register(Role)
-admin.site.register(Permission)
-admin.site.register(DivisionMaster)
+# admin.site.register(Role)
+# admin.site.register(Permission)
+# admin.site.register(DivisionMaster)
 admin.site.register(FileType)
 admin.site.register(FileClassification)
 admin.site.register(CaseStatus)
+
+@admin.register(DivisionMaster)
+class DivisionMasterAdmin(admin.ModelAdmin):
+    search_fields = ['divsionId','divisionName','active']
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    search_fields = ['roleName']
+
+@admin.register(DesignationMaster)
+class DesignationMasterAdmin(admin.ModelAdmin):
+    search_fields = ['designationName']
--- RRMSAPI/users/admin.py
+++ RRMSAPI/users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import User, UserDivisionRole
 
+class UserDivisionRoleInline(admin.TabularInline):  # or admin.StackedInline
+    model = UserDivisionRole
+    extra = 1
+    autocomplete_fields = ['division', 'role', 'designation']  # improves usability
+    show_change_link = True
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['kgid','first_name', 'last_name', 'email','is_staff','last_login']
+    readonly_fields = ['is_staff']
+    inlines = [UserDivisionRoleInline]
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(UserDivisionRole)
+admin.site.register(User, CustomUserAdmin)
+# admin.site.register(UserDivisionRole)
