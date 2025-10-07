5d6a960ba84ec66bd09fee2feb5e1311977b0a03
Gaya3budha <budhagayatri95@gmail.com>
2025-10-08 02:47:56 +0530

    render deploy issue fix

--- RRMSAPI/users/utils.py
+++ RRMSAPI/users/utils.py
@@ -7,6 +7,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+import os
 
 def send_password_setup_email(user):
     token = default_token_generator.make_token(user)
